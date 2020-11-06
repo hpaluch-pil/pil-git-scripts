4df81146bca89fd841496903c4cb9b7d64dbe6cc
Henryk Paluch <henryk.paluch@pickering.cz>
2020-11-06 10:02:02 +0100

    Now can build both binary and source rpms

--- build_rpm.sh
+++ build_rpm.sh
@@ -2,6 +2,35 @@
 
 set -e
 set -o pipefail
+
+usage () {
+	echo "Usage: $0 [ all|binary|source ]" >&2
+	echo " where: " >&2
+	echo "   all (default) - build both binary and source rpm" >&2
+	echo "   binary        - build only binary rpm" >&2
+	echo "   source        - build both binary and source rpm" >&2
+	exit 1
+}
+
+declare -A build_args
+build_args["all"]=-ba
+build_args["binary"]=-bb
+build_args["source"]=-bs
+
+build_type=all
+[ $# -lt 2 ] || usage
+
+[ $# -eq 0 ] || build_type="$1"
+# remove any slashes
+build_type=${build_type//-/}
+
+# it is weird but these nested quotes really work...
+[ -n "${build_args["$build_type"]}" ]  || {
+	echo "ERROR: Invalid build type '$build_type'" >&2
+	echo 
+	usage
+}
+
 cd `dirname $0`
 source /etc/os-release
 rm -rf build/rpm-$ID
@@ -24,9 +53,11 @@ cp -r scripts build/rpm-$ID/tree/pil-git-scripts
 ( cd build/rpm-$ID/tree && tar cvzf ../rpmbuild/SOURCES/pil-git-scripts.tar.gz \
         pil-git-scripts/ )
 
-rpmbuild -bb \
+set -x
+rpmbuild ${build_args["$build_type"]} \
 	-D'_topdir '`pwd`/build/rpm-$ID/rpmbuild \
 	-D'_tmppath '`pwd`/build/rpm-$ID/rpmbuild/tmp \
        	pil-git-scripts.spec
+set +x
 exit 0
 
